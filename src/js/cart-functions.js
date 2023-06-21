5b9944a9676fa4ba2dd412e74105a12d7d5e2598
Akhirahstrong <codeclubdetroit@gmail.com>
2023-06-21 11:45:19 -0400

    3 tests

---
+++ src/js/cart-functions.js
@@ -0,0 +1,55 @@
+/*calculateChange function
+In the file cart-functions.js, create a calculateChange function and export it.
+This function has two parameters:
+total (a number)
+payment (a number)
+The function returns the difference between the payment and the total.
+*/
+
+function calculateChange(total, payment) {
+  let difference = payment - total;
+  return difference;
+}
+
+exports.calculateChange = calculateChange;
+
+/*isSufficientPayment function
+In the file cart-functions.js, 
+create a isSufficientPayment function and export it.
+This function has two parameters:
+total (a number)
+payment (a number)
+The function returns true if payment is greater 
+than or equal to total. It returns false otherwise.*/
+
+function isSufficientPayment(total, payment) {
+  if (payment >= total) {
+    return true;
+  } else {
+    return false;
+  }
+}
+exports.isSufficientPayment = isSufficientPayment;
+
+/*calculateTotal function
+In the file cart-functions.js, create a calculateTotal 
+function and export it.
+This function has one parameter:
+itemsArray (an array of objects)
+Each object in the itemsArray will have a name and a price. 
+Here's an example
+{ name: "Jelly", price: 3.50 }
+The function returns the sum of the prices of all objects 
+in the array. The names of the items do not affect this function.*/
+
+function calculateTotal(itemsArray) {
+  let sum = 0;
+
+  for (let i = 0; i < itemsArray.legth; i++) {
+    sum += itemsArray[i].price;
+  }
+
+  return sum;
+}
+
+exports.calculateTotal = calculateTotal;
